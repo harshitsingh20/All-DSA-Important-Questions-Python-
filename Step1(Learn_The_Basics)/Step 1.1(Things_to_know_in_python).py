f1bd9ba9ba41571977a4f5b1891d2ecf89b7d2ec
Harshit <harshitsingh2021@gmail.com>
2022-12-11 11:21:23 +0530

    UserInput(Step 1.1) Done

---
+++ Step1(Learn_The_Basics)/Step 1.1(Things_to_know_in_python).py
@@ -0,0 +1,19 @@
+
+# User Input
+
+# Examples
+
+# Taking input from the user (It's takes input as a String)
+n = input("Enter your n: ")
+
+# Output
+print("Hello, " + n)
+print(type(n))
+
+
+# Taking input from the user (It's takes input as a Number)
+a = int(input("Enter your number: "))
+
+# Output
+print("Number is, " , a)
+print(type(a))
